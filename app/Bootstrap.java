a54c31aa07137e985b867ebcf94c3b8dd8fd7295
John Simone <john@heroku.com>
2012-02-23 19:07:48 -0800

    update bootstrap file

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -9,7 +9,7 @@ public class Bootstrap extends Job {
 
     public void doJob() {
         // Check if the database is empty
-        if(Tweet.count() == 0) {
+        if(Tweet.count() == 0 && User.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
         }
     }
